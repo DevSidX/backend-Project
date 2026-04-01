896e4a9940425ee494d662d2b48a878c27cb7aa7
SIDDHARTH <121356793+DevSidX@users.noreply.github.com>
2026-04-02 01:10:34 +0530

    bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -122,7 +122,7 @@ const loginUser = asyncHandler( async (req, res) => {
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
-    if(isPasswordValid){
+    if(!isPasswordValid){
         throw new ApiError(401, "Incorrect password"); // or invalid user credentials
     }
 
