be58e5ae530b5b8e35907b543312126df99afbba
SIDDHARTH <121356793+DevSidX@users.noreply.github.com>
2026-04-09 21:24:12 +0530

    implement video retrieval with filters, pagination, and sorting; add video update and delete functionalities; toggle publish status

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -7,11 +7,90 @@ import { uploadOnCloudinary } from "../utils/fileUpload.js";
 
 // TODO: get all the videos based on query, sort, pagination
 const getAllVideos = asyncHandler ( async (req,res) => {
+    // fetch all the videos with applied filters from the Video collection
+    // apply pagination - return data page wise not all at once
+    // apply search (optional) - if query is given then find videos where title matches or description matches
+    // apply sorting (optional) - based on sortBy and sortType
+    // Filter by user (optional) - if userId is return the videos which are uploaded by the user
+    // return only published videos (APPLY IN ALL)
+    
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const pageNum = parseInt(page)
+    const limitNum = parseInt(limit)
+
+    if(pageNum < 1 || limitNum < 1){
+        throw new ApiError(400, "Invalid page or limit!");
+    }
+
+    if(userId && !mongoose.isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid userId");
+    }
+
+    // Filter - Only fetch published videos
+    const match = {
+        isPublished: true
+    };
+
+    // search query
+    if(query && query.trim() !== ""){
+        match.$or = [  // logical $or return if even one condition satisfies either title or description
+            {
+                title: {
+                    $regex : query,
+                    $options: "i"
+                }
+            },
+            {
+                description: {
+                    $regex: query,
+                    $options: "i"
+                }
+            }
+        ];
+    }
+
+    // filter - videos by specific users
+    if(userId){
+        match.owner = new mongoose.Types.ObjectId(userId)
+    }
+
+    // sort the results
+    const sort = {
+        [sortBy || "createdAt"]: sortType === "asc" ? 1 : -1
+    };
+
+    // aggregation
+    const aggregate = Video.aggregate([
+        {
+            $match: match
+        },
+        {
+            $sort: sort
+        }
+    ])
+
+    // pagination
+    const videos = await Video.aggregatePaginate(
+        aggregate,
+        {
+            page: pageNum,
+            limit: limitNum
+        }
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            videos,
+            "Videos fetched successfully"
+        )
+    )
 })
 
 // TODO: get video, upload to cloudinary, create video
-const publishAVideo = asyncHandler ( async (req,res) => {
+const publishAVideo = asyncHandler ( async (req, res) => {
     const { title, description } = req.body
 
     if(!title?.trim()){
@@ -100,6 +179,103 @@ const getVideoById = asyncHandler(async (req, res) => {
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
 
+    if(!mongoose.isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Video id");
+    }
+
+    const { title, description } = req.body
+    const thumbnail = req.files?.thumbnail?.[0]?.path
+
+    const updateDetails = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set: {
+                ...(title !== undefined && { title }),  // Only add the title property if title is not undefined
+                ...(description !== undefined && { description }),  // same
+                ...(thumbnail !== undefined && { thumbnail })  // same
+            }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+
+    if(!updateDetails){
+        throw new ApiError(404, "Video not found");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updateDetails,
+            "Video details updated successfully!"
+        )
+    )
 })
 
-export { getAllVideos, publishAVideo }
+//TODO: delete video
+const deleteVideo = asyncHandler (async (req, res) => {
+    const { videoId } = req.params
+
+    if(!mongoose.isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video ID");
+    }
+
+    const deleteVideo = await Video.findByIdAndDelete(videoId)
+
+    if(!deleteVideo){
+        throw new ApiError(404, "Video not found!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            deleteVideo,
+            "Video deleted successfully"
+        )
+    )
+})
+
+const togglePublishStatus = asyncHandler (async (req,res) => {
+    const { videoId } = req.params
+
+    if(mongoose.isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid video Id");
+    }
+
+    const video = await Video.findById(videoId)
+
+    if(!video){
+        throw new ApiError(404, "Video not found");
+    }
+
+    video.isPublished = !video.isPublished  // changed status
+
+    await video.save({
+        validateBeforeSave: false
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            video,
+            "Publish status toggled successfully!"
+        )
+    )
+})
+
+export { 
+    getAllVideos, 
+    publishAVideo, 
+    getVideoById ,
+    updateVideo, 
+    deleteVideo, 
+    togglePublishStatus 
+}
